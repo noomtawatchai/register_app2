39df7b255175a2057e170c859e8607e600b5f6e9
noomtawatchai <58242338+noomtawatchai@users.noreply.github.com>
2020-08-20 11:17:16 +0700

    #5 Menu Bar

--- vueapp3/src/App.vue
+++ vueapp3/src/App.vue
@@ -1,20 +1,59 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app>
-      <p>navigation-drawer</p>
-    </v-navigation-drawer>
+  <!-- App.vue -->
 
+  <v-app>
+    <!-- เมนูด้านข้าง -->
+      <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  
     <v-app-bar app>
-      <p>app-ber</p>
+      <!-- -->
+      <p>app bar</p>
     </v-app-bar>
 
+    <!-- Sizes your content based upon application components -->
     <v-main>
+      <!-- Provides the application the proper gutter -->
       <v-container fluid>
+        <!-- <p>main</p> --> 
+        <!-- If using vue-router -->
         <router-view></router-view>
       </v-container>
     </v-main>
 
     <v-footer app>
+      <!-- -->
       <p>footer</p>
     </v-footer>
   </v-app>
@@ -24,10 +63,17 @@
 export default {
   data() {
     return {
+      // ไอคอนเมนู
+      items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
       message: "RMUTL",
-    };
+    }
   },
-};
+}
 </script>
 
 <style>
